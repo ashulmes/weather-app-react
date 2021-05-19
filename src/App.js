1c2053eaca3678822166d29dc6fc708c8f0addbc
ashulmes <ashulmes@gmail.com>
2021-05-19 22:46:00 +0100

    Made API call for weather data and added spinner on load.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <div className="container">
       <div className="app-contents">
-        <Weather />
+        <Weather defaultCity="Manchester" />
         <Forecast />
       </div>
       <Footer />
--- src/Weather.js
+++ src/Weather.js
@@ -1,74 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import Loader from "react-loader-spinner";
 import "./App.css";
 import "./Weather.css";
 
-export default function Weather() {
-  let weatherData = {
-    city: "Manchester",
-    date: "24 Apr, 19:55",
-    description: "Clear Sky",
-    icon: "http://openweathermap.org/img/wn/01d@2x.png",
-    temperature: 18,
-    feelsLike: 12,
-    wind: 7,
-    humidity: 14,
-  };
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ loaded: false });
 
-  return (
-    <div>
-      <div className="search-form">
-        <form>
-          <div className="row">
-            <div className="col-10 search-field">
-              <input
-                type="text"
-                className="form-control"
-                autoComplete="off"
-                autoFocus
-                placeholder="Enter a city..."
-              />
-            </div>
-            <div className="col-1 search-button">
-              <button type="submit" className="btn btn-secondary">
-                <i className="fas fa-search"></i>
-              </button>
-            </div>
-            <div class="col-1 locate-button">
-              <button type="button" className="btn btn-secondary">
-                <i className="fas fa-location-arrow"></i>
-              </button>
-            </div>
-          </div>
-        </form>
-      </div>
+  function handleResponse(response) {
+    setWeatherData({
+      loaded: true,
+      city: response.data.name,
+      date: "24 Apr, 19:55",
+      description: response.data.weather[0].description,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      temperature: response.data.main.temp,
+      feelsLike: response.data.main.feels_like,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+    });
+  }
 
+  if (weatherData.loaded) {
+    return (
       <div>
-        <h1>{weatherData.city}</h1>
-        <p>Updated: {weatherData.date}</p>
-        <img
-          src={weatherData.icon}
-          className="weather-icon"
-          alt={weatherData.description}
-        />
-        <span className="temperature-value">{weatherData.temperature}</span>
-        <span className="temperature-metric">°C</span>
-        <p className="weather-description">{weatherData.description}</p>
+        <div className="search-form">
+          <form>
+            <div className="row">
+              <div className="col-10 search-field">
+                <input
+                  type="text"
+                  className="form-control"
+                  autoComplete="off"
+                  autoFocus
+                  placeholder="Enter a city..."
+                />
+              </div>
+              <div className="col-1 search-button">
+                <button type="submit" className="btn btn-secondary">
+                  <i className="fas fa-search"></i>
+                </button>
+              </div>
+              <div class="col-1 locate-button">
+                <button type="button" className="btn btn-secondary">
+                  <i className="fas fa-location-arrow"></i>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
 
-        <section className="weather-info">
-          <div className="row justify-content-center headings">
-            <div className="col-3">Feels Like</div>
-            <div className="col-3">Wind</div>
-            <div className="col-3">Humidity</div>
-          </div>
+        <div>
+          <h1>{weatherData.city}</h1>
+          <p>Updated: {weatherData.date}</p>
+          <img
+            src={weatherData.icon}
+            className="weather-icon"
+            alt={weatherData.description}
+          />
+          <span className="temperature-value">
+            {Math.round(weatherData.temperature)}
+          </span>
+          <span className="temperature-metric">°C</span>
+          <p className="weather-description">{weatherData.description}</p>
 
-          <div className="row justify-content-center">
-            <div className="col-3">{weatherData.feelsLike}°C</div>
-            <div className="col-3">{weatherData.wind} MPH</div>
-            <div className="col-3">{weatherData.humidity}%</div>
-          </div>
-        </section>
+          <section className="weather-info">
+            <div className="row justify-content-center headings">
+              <div className="col-3">Feels Like</div>
+              <div className="col-3">Wind</div>
+              <div className="col-3">Humidity</div>
+            </div>
+
+            <div className="row justify-content-center">
+              <div className="col-3">{Math.round(weatherData.feelsLike)}°C</div>
+              <div className="col-3">{Math.round(weatherData.wind)} MPH</div>
+              <div className="col-3">{weatherData.humidity}%</div>
+            </div>
+          </section>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "4a150b550611ee8a27e04e337620852b";
+    const units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return (
+      <Loader
+        type="TailSpin"
+        color="#aebcdb"
+        height={100}
+        width={100}
+        timeout={3000}
+      />
+    );
+  }
 }
